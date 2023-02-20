76522855e13e8c497ac0fcd67c6d4347b2595537
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-20 18:10:37 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types'
 import styles from './ImageGalleryItem.module.css'
 
-export function ImageGalleryItem({ webformatURL, largeImageURL, onOpen }) {
+export function ImageGalleryItem({ card, onOpen }) {
     return (
         <li className={styles.gallery_item}>
-        <img className={styles.gallery_item_image} src={webformatURL} alt="" onClick={()=>onOpen(largeImageURL)} />
+        <img className={styles.gallery_item_image} src={card.webformatURL} alt="" onClick={()=>onOpen(card)} />
         </li>
     )
 }
