c4db6e4ff8bcca230778f7732584cea0e929ab1d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-20 22:48:13 +0100

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,30 +1,32 @@
-import PropTypes from 'prop-types'
-import { Component } from 'react'
-import styles from './SearchBar.module.css'
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from './SearchBar.module.css';
 
-export class Searchbar extends Component {
+export const Searchbar =( {handleSubmit, onInputChange, search})=>{
     
-    render() {
+
         return (
             <header className={styles.searchbar_header}>
-                <form className={styles.form} onSubmit={this.props.onSubmit}>
+                <form className={styles.form} onSubmit={handleSubmit}>
                     <button type="submit" className={styles.form_button}>&#x1F50D;</button>
 
                     <input 
-                        onChange={this.props.onInputChange}
+                        onChange={onInputChange}
                         className={styles.form_input}
                         name='searchInput'
                         type="text"
                         autoComplete="off"
                         autoFocus
                         placeholder="Search images and photos"
+                        value={search}
                     />
                 </form>
             </header>
-        )
-    }
+        );
 }
-
+  
 Searchbar.propTypes = {
-    onSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    onInputChange:PropTypes.func.isRequired,
+    search: PropTypes.string.isRequired,
 }
