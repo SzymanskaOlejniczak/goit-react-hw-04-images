2319f1d130ef1f1a73c9e0f559cd91c93d0b9545
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:26 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,21 +1,22 @@
-import PropTypes from 'prop-types'
-import styles from './ImageGalleryItem.module.css'
+import PropTypes from 'prop-types';
+import styles from './ImageGalleryItem.module.css';
 
 function ImageGalleryItem({ webformatURL, largeImageURL, onOpen }) {
-    return (
-        <li className={styles.gallery_item}>
-        <img 
-        className={styles.gallery_item_image} 
-        src={webformatURL} 
-        alt="" 
-        onClick={()=>onOpen(largeImageURL)} />
-        </li>
-    )
+  return (
+    <li className={styles.gallery_item}>
+      <img
+        className={styles.gallery_item_image}
+        src={webformatURL}
+        alt=""
+        onClick={() => onOpen(largeImageURL)}
+      />
+    </li>
+  );
 }
 
 ImageGalleryItem.propTypes = {
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    onOpen: PropTypes.func.isRequired
-    }
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  onOpen: PropTypes.func.isRequired,
+};
 export default ImageGalleryItem;
