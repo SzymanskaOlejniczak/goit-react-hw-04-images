4ec1329dce16242f505a99374c16931103d906a8
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:08 +0100

    Update api.js

--- src/API/api.js
+++ src/API/api.js
@@ -1,21 +1,18 @@
-import axios from "axios";
-
+import axios from 'axios';
 
 export const fetchPhotos = async (search, numPage) => {
-    const response = await axios.get(`https://pixabay.com/api/`, 
-    {
-        method: "get",
-        params: {
-            key: "31279594-d7f31148a9794d6f86efa6037",
-            q: search,
-            image_type: "photo",
-            orientation: "horizontal",
-            safesearch: true,
-            per_page: 12,
-            page: numPage,
-        },
-    });
-   
-    return response.data.hits
-    
-}
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    method: 'get',
+    params: {
+      key: '31279594-d7f31148a9794d6f86efa6037',
+      q: search,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: numPage,
+    },
+  });
+
+  return response.data.hits;
+};
