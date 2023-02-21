96d88ca118cbc47e7343cf6bb36c194491e6fe08
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:21 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,29 +1,29 @@
-import PropTypes from 'prop-types'
-import  ImageGalleryItem  from '../ImageGalleryItem/ImageGalleryItem'
-import styles from './ImageGallery.module.css'
+import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import styles from './ImageGallery.module.css';
 
-export function ImageGallery ({cards, onClick}) {
-    return (
-        <ul className={styles.image_gallery}>
-             {cards.map(({webformatURL, largeImageURL, id}) => (
-                    <ImageGalleryItem 
-                        key={id} 
-                        webformatURL={webformatURL} 
-                        largeImageURL={largeImageURL} 
-                        onOpen={onClick} 
-                    />
-                )
-             )
-            }
-        </ul>
-    );
-};
+export function ImageGallery({ cards, onClick }) {
+  return (
+    <ul className={styles.image_gallery}>
+      {cards.map(({ webformatURL, largeImageURL, id }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          onOpen={onClick}
+        />
+      ))}
+    </ul>
+  );
+}
 
 ImageGallery.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    cards: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        webformatURL: PropTypes.string.isRequired,
-        largeImageURL: PropTypes.string.isRequired
-    }))
-}
+  onClick: PropTypes.func.isRequired,
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
+};
