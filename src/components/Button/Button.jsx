f4f20b98b3482e7cc7d0ed65bc111222f369dee9
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:15 +0100

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,14 @@
-import PropTypes from 'prop-types'
-import styles from './Button.module.css'
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
 
 export function Button({ onClick }) {
-    return (
-        <button 
-            onClick={onClick} 
-            className={styles.loader_button}>
-            Load more
-        </button>
-    )
+  return (
+    <button onClick={onClick} className={styles.loader_button}>
+      Load more
+    </button>
+  );
 }
 
 Button.propTypes = {
-    onClick: PropTypes.func.isRequired,
-}
+  onClick: PropTypes.func.isRequired,
+};
