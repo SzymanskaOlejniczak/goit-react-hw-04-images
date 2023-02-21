3382fcec42001a97913d1bfeb6bdc553376208b1
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:37 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import styles from './Modal.module.css';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-export function Modal ({onClose, card}){
-    useEffect (()=> {
-        const handleKeyDown = e => {
-                if (e.code === 'Escape') {
-                return onClose();
-                }
-        };
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, [onClose]);
-  
-        return (
-        <div className={styles.overlay} onClick={onClose}>
-            <div className={styles.modal}>
-                <img src={card} alt="modalPicture" />
-            </div>
-        </div>
-        );
+export function Modal({ onClose, card }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        return onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modal}>
+        <img src={card} alt="modalPicture" />
+      </div>
+    </div>
+  );
 }
 
-
 Modal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    card: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  card: PropTypes.string.isRequired,
 };
