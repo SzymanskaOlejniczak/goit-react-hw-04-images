e2030174b8a0e1aa3ae1dd3318873fd28bea9bcd
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2023-02-21 19:50:42 +0100

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,31 +2,31 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './SearchBar.module.css';
 
-export const Searchbar =( {handleSubmit, onInputChange, search})=>{
-    
+export const Searchbar = ({ handleSubmit, onInputChange, search }) => {
+  return (
+    <header className={styles.searchbar_header}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <button type="submit" className={styles.form_button}>
+          &#x1F50D;
+        </button>
 
-        return (
-            <header className={styles.searchbar_header}>
-                <form className={styles.form} onSubmit={handleSubmit}>
-                    <button type="submit" className={styles.form_button}>&#x1F50D;</button>
+        <input
+          onChange={onInputChange}
+          className={styles.form_input}
+          name="searchInput"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={search}
+        />
+      </form>
+    </header>
+  );
+};
 
-                    <input 
-                        onChange={onInputChange}
-                        className={styles.form_input}
-                        name='searchInput'
-                        type="text"
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                        value={search}
-                    />
-                </form>
-            </header>
-        );
-}
-  
 Searchbar.propTypes = {
-    handleSubmit: PropTypes.func.isRequired,
-    onInputChange:PropTypes.func.isRequired,
-    search: PropTypes.string.isRequired,
-}
+  handleSubmit: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  search: PropTypes.string.isRequired,
+};
